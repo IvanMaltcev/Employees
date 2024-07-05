e7ea9520513a1f556c1aa2d8ea77b9f02a190797
user <ivan777maltsev@yandex.ru>
2024-07-05 19:42:39 +0300

    Add library commons-lang3 and check string data

--- src/main/java/com/sky/employee/service/EmployeeServiceImp.java
+++ src/main/java/com/sky/employee/service/EmployeeServiceImp.java
@@ -1,9 +1,11 @@
 package com.sky.employee.service;
 
+import com.sky.employee.exception.EmployeeIsNotValidException;
 import com.sky.employee.exception.EmployeeNotFoundException;
 import com.sky.employee.exception.EmployeeStorageIsFullException;
 import com.sky.employee.exception.EmployeeAlreadyAddedException;
 import com.sky.employee.model.Employee;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -20,24 +22,35 @@ public class EmployeeServiceImp implements EmployeeService {
 
     @Override
     public Employee addEmployee(String firstName, String lastName, int salary, int department) {
-        if (employees.size() >= MAX_AMOUNT_EMPLOYEES) {
 
-            throw new EmployeeStorageIsFullException("Превышен лимит количества сотрудников в фирме!");
-        }
-        final Employee employee = new Employee(firstName, lastName, salary, department);
-        if (employees.containsKey(employee.getFullName())) {
-            throw new EmployeeAlreadyAddedException("Такой сотрудник уже существует!");
-        } else {
-            employees.put(employee.getFullName(), employee);
-        }
-        return employees.get(employee.getFullName());
+        String validFirstName = validationStringData(firstName);
+        String validLastName = validationStringData(lastName);
+
+            if (employees.size() >= MAX_AMOUNT_EMPLOYEES) {
+
+                throw new EmployeeStorageIsFullException("Превышен лимит количества сотрудников в фирме!");
+            }
+
+            final Employee employee = new Employee(validFirstName, validLastName, salary, department);
+
+            if (employees.containsKey(employee.getFullName())) {
+                throw new EmployeeAlreadyAddedException("Такой сотрудник уже существует!");
+            } else {
+                employees.put(employee.getFullName(), employee);
+            }
+            return employees.get(employee.getFullName());
+
 
     }
 
     @Override
     public Employee removeEmployee(String firstName, String lastName) {
+
+        String validFirstName = validationStringData(firstName);
+        String validLastName = validationStringData(lastName);
+
         return employees.remove(employees.values().stream()
-                .filter(e -> e.getFullName().equals(firstName + " " + lastName))
+                .filter(e -> e.getFullName().equals(validFirstName + " " + validLastName))
                 .findAny()
                 .orElseThrow(() -> new EmployeeNotFoundException("Сотрудник не найден!"))
                 .getFullName());
@@ -46,8 +59,12 @@ public class EmployeeServiceImp implements EmployeeService {
 
     @Override
     public Employee findEmployee(String firstName, String lastName) {
+
+        String validFirstName = validationStringData(firstName);
+        String validLastName = validationStringData(lastName);
+
         return employees.values().stream()
-                .filter(e -> e.getFullName().equals(firstName + " " + lastName))
+                .filter(e -> e.getFullName().equals(validFirstName + " " + validLastName))
                 .findAny()
                 .orElseThrow(() -> new EmployeeNotFoundException("Сотрудник не найден!"));
     }
@@ -55,5 +72,13 @@ public class EmployeeServiceImp implements EmployeeService {
     @Override
     public Collection<Employee> getListOfEmployees() {
         return Collections.unmodifiableCollection(employees.values());
+    }
+
+    private String validationStringData (String string) {
+        if (StringUtils.isAlpha(string)) {
+            return StringUtils.capitalize(string.toLowerCase());
+        } else {
+            throw new EmployeeIsNotValidException("Не верно введены имя или фамилия");
+        }
     }
 }
